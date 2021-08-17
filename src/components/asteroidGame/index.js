06d9d0c1e5259c16acd8ca5063cf3bedb2ca390c
Chris Cho <chrischo360@gmail.com>
2021-08-17 09:46:19 -0400

    web20 changes

--- src/components/asteroidGame/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Reacteroids } from './Reacteroids';
-import style from './style.css';
--- src/pages/Publications.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { PublicationsGrid } from '../components/PublicationsGrid';
-import { PUBLICATIONS_LIST } from '../utils/data/researchInformation';
-
-export const Publications: React.VFC = () => (
-  <PublicationsGrid title="Publications" list={PUBLICATIONS_LIST} />
-);
